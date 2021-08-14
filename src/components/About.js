b822034a257a57041df1177dea89f8d98111a109
tnminhlc <60195770+tnminhlc@users.noreply.github.com>
2021-08-14 04:46:25 -0700

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
+    <div align='center'>
       <h4>Educational Solution for TecHacks 2021</h4>
       <Link to='/'>Go Back</Link>
     </div>
